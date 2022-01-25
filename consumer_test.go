dadcb67e912af9aaf1ee1311b432ceb27e45d7ba
AdamKorcz <adam@adalogics.com>
2022-01-25 21:22:12 +0000

    Add api for getting runes

--- consumer_test.go
+++ consumer_test.go
@@ -1,24 +1,24 @@
 package gofuzzheaders
 
 import (
-	//"fmt"
+	"fmt"
 	"testing"
 )
 
 type TestStruct1 struct {
-	Field1          string
-	Field2   		string
-	Field3			[]byte
-	field4			string
+	Field1 string
+	Field2 string
+	Field3 []byte
+	field4 string
 }
 
 func TestStruct_fuzzing1(t *testing.T) {
 	data := []byte{0x02, 0x41, 0x42, // Field1
-				   0x03, 0x41, 0x42, 0x43, // Field2
-				   0x04, // Length of byte slice via make()
-				   0x41, 0x42, 0x43, 0x44, // Field 3
-				   0x02, 0x41, 0x42} // FIeld4
-	
+		0x03, 0x41, 0x42, 0x43, // Field2
+		0x04,                   // Length of byte slice via make()
+		0x41, 0x42, 0x43, 0x44, // Field 3
+		0x02, 0x41, 0x42} // FIeld4
+
 	ts1 := TestStruct1{}
 	fuzz1 := NewConsumer(data)
 	err := fuzz1.GenerateStruct(&ts1)
@@ -26,16 +26,16 @@ func TestStruct_fuzzing1(t *testing.T) {
 		t.Errorf("%v", err)
 	}
 	//fmt.Printf("%+v\n", ts1)
-	if ts1.Field1!="AB" {
+	if ts1.Field1 != "AB" {
 		t.Errorf("ts1.Field1 was %v but should be 'AB'", ts1.Field1)
 	}
-	if ts1.Field2!="ABC" {
+	if ts1.Field2 != "ABC" {
 		t.Errorf("ts1.Field2 was %v but should be 'ABC'", ts1.Field2)
 	}
-	if string(ts1.Field3)!="ABCD" {
+	if string(ts1.Field3) != "ABCD" {
 		t.Errorf("ts1.Field3 was %v but should be 'ABCD'", ts1.Field3)
 	}
-	if string(ts1.field4)!="" {
+	if string(ts1.field4) != "" {
 		t.Errorf("ts1.field4 was %v but should be empty", ts1.field4)
 	}
 	ts2 := TestStruct1{}
@@ -45,84 +45,84 @@ func TestStruct_fuzzing1(t *testing.T) {
 	if err != nil {
 		t.Errorf("%v", err)
 	}
-	if string(ts2.field4)!="AB" {
+	if string(ts2.field4) != "AB" {
 		t.Errorf("ts2.field4 was %v but should be 'AB'", ts2.field4)
 	}
 }
 
 type TestStruct2 struct {
-	Struct2Field1   string
-	Struct2Field2   string
+	Struct2Field1 string
+	Struct2Field2 string
 }
 
 type TestStruct3 struct {
-	Field1          string
-	Field2   		string
-	Field3 			*TestStruct2
+	Field1 string
+	Field2 string
+	Field3 *TestStruct2
 }
 
 func TestStruct_fuzzing2(t *testing.T) {
 	data := []byte{0x02, 0x41, 0x42, // Field1
-				   0x03, 0x41, 0x42, 0x43, // Field2
-				   0x04, 0x41, 0x42, 0x43, 0x44, // Field 3
-				   0x02, 0x41, 0x42} // FIeld4
+		0x03, 0x41, 0x42, 0x43, // Field2
+		0x04, 0x41, 0x42, 0x43, 0x44, // Field 3
+		0x02, 0x41, 0x42} // FIeld4
 	ts3 := TestStruct3{}
 	fuzz1 := NewConsumer(data)
 	err := fuzz1.GenerateStruct(&ts3)
 	if err != nil {
 		t.Errorf("%v", err)
 	}
-	if string(ts3.Field1)!="AB" {
+	if string(ts3.Field1) != "AB" {
 		t.Errorf("ts3.Field1 was %v but should be 'AB'", ts3.Field1)
 	}
-	if string(ts3.Field2)!="ABC" {
+	if string(ts3.Field2) != "ABC" {
 		t.Errorf("ts3.Field2 was %v but should be 'AB'", ts3.Field2)
 	}
-	if string(ts3.Field3.Struct2Field1)!="ABCD" {
+	if string(ts3.Field3.Struct2Field1) != "ABCD" {
 		t.Errorf("ts3.Field3.Struct2Field1 was %v but should be 'ABCD'", ts3.Field3.Struct2Field1)
 	}
-	if string(ts3.Field3.Struct2Field2)!="AB" {
+	if string(ts3.Field3.Struct2Field2) != "AB" {
 		t.Errorf("ts3.Field3.Struct2Field2 was %v but should be 'AB'", ts3.Field3.Struct2Field2)
 	}
 }
 
 func TestFuzzMap1(t *testing.T) {
 	data := []byte{0x02, // Length of map
-				   0x04, 0x4B, 0x65, 0x79, 0x31, // "Key1"
-				   0x04, 0x56, 0x61, 0x6C, 0x31, // "Val1"
-				   0x04, 0x4B, 0x65, 0x79, 0x32, // "Key2"
-				   0x04, 0x56, 0x61, 0x6C, 0x32} // "Val2"
-	var m map[string]string	
+		0x04, 0x4B, 0x65, 0x79, 0x31, // "Key1"
+		0x04, 0x56, 0x61, 0x6C, 0x31, // "Val1"
+		0x04, 0x4B, 0x65, 0x79, 0x32, // "Key2"
+		0x04, 0x56, 0x61, 0x6C, 0x32} // "Val2"
+	var m map[string]string
 	fuzz1 := NewConsumer(data)
 	err := fuzz1.FuzzMap(&m)
 	if err != nil {
 		t.Errorf("%v", err)
 	}
-	if m["Key1"]!="Val1" {
+	if m["Key1"] != "Val1" {
 		t.Errorf("m[\"Key1\"] should be \"Val1\" but should be")
 	}
-	if m["Key2"]!="Val2" {
+	if m["Key2"] != "Val2" {
 		t.Errorf("m[\"Key2\"] should be \"Val2\" but should be")
 	}
 }
 
 func TestFuzzMap2(t *testing.T) {
 	data := []byte{0x02, // Length of map
-				   0x04, 0x4B, 0x65, 0x79, 0x31, // "Key1"
-				   0x04, 0x56, 0x61, 0x6C, 0x31, // "Val1"
-				   0x04, 0x4B, 0x65, 0x79, 0x32, // "Key2"
-				   0x04, 0x56, 0x61, 0x6C, 0x32} // "Val2"
-	var m map[string][]byte	
+		0x04, 0x4B, 0x65, 0x79, 0x31, // "Key1"
+		0x04, 0x56, 0x61, 0x6C, 0x31, // "Val1"
+		0x04, 0x4B, 0x65, 0x79, 0x32, // "Key2"
+		0x04, 0x56, 0x61, 0x6C, 0x32} // "Val2"
+	var m map[string][]byte
 	fuzz1 := NewConsumer(data)
 	err := fuzz1.FuzzMap(&m)
 	if err != nil {
 		t.Errorf("%v", err)
 	}
-	
-	if string(m["Key1"])!="Val1" {
+
+	if string(m["Key1"]) != "Val1" {
 		t.Errorf("m[\"Key1\"] should be \"Val1\" but should be")
 	}
-	if string(m["Key2"])!="Val2" {
+	if string(m["Key2"]) != "Val2" {
 		t.Errorf("m[\"Key2\"] should be \"Val2\" but should be")
 	}
 }
@@ -134,7 +134,7 @@ func TestFuzzGetStringFrom(t *testing.T) {
 	if err != nil {
 		t.Errorf("Got an error here but shouldn't")
 	}
-	if createdString!="345678" {
+	if createdString != "345678" {
 		t.Errorf("Created string should have been 345678")
 	}
 
@@ -144,7 +144,7 @@ func TestFuzzGetStringFrom(t *testing.T) {
 	if err != nil {
 		t.Errorf("Got an error but shouldn't")
 	}
-	if createdString!="ijklmnkl" {
+	if createdString != "ijklmnkl" {
 		t.Errorf("Created string should have been ijklmnkl")
 	}
 }
@@ -156,7 +156,7 @@ func TestUint16(t *testing.T) {
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	if u16!=32623 {
+	if u16 != 32623 {
 		t.Errorf("the u16 should be 32623")
 	}
 
@@ -166,8 +166,21 @@ func TestUint16(t *testing.T) {
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	if u16!=28543 {
+	if u16 != 28543 {
 		t.Errorf("the u16 should be 28543")
 	}
 
+}
+
+func TestGetRune(t *testing.T) {
+	data := []byte{0x3, 0x41, 0x42, 0x43}
+	fuzz1 := NewConsumer(data)
+	newRune, err := fuzz1.GetRune()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	fmt.Println(newRune)
+	if string(newRune) != "ABC" {
+		t.Errorf("'newRune' should be 'ABC', but is something else")
+	}
 }
