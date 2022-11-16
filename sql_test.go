480a3b2a9c86de6e628cc5c01ea90615025f103b
Sebastiaan van Stijn <github@gone.nl>
2022-11-16 12:25:56 +0100

    TestSQLApi: use subtests, and regular t.Error/t.Fail
    Also renaming to TestSQLAPI to follow Go's camelCase rules.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- sql_test.go
+++ sql_test.go
@@ -1,30 +1,32 @@
 package gofuzzheaders
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestSQLApi(t *testing.T) {
-	data := []byte{1, 1, 0, 1}
-	f := NewConsumer(data)
-	query, err := f.GetSQLString()
-	if err != nil {
-		panic(err)
-	}
-	if query != " action" {
-		panic("Should be ' action'")
-	}
-	fmt.Println("Test 2")
-	data2 := []byte{
-		222, 255, 0, 100, 10, 64, 2, 0, 0, 0,
-		0, 0, 0, 100, 6, 0, 0, 0, 0, 0, 0, 255,
-		61, 100, 170, 0, 0,
-	}
-	f2 := NewConsumer(data2)
-	query, err = f2.GetSQLString()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(query)
+func TestSQLAPI(t *testing.T) {
+	t.Run("Test 1", func(t *testing.T) {
+		data := []byte{1, 1, 0, 1}
+		f := NewConsumer(data)
+		query, err := f.GetSQLString()
+		if err != nil {
+			t.Error(err)
+		}
+		if query != " action" {
+			t.Errorf("expected ' action', got: '%s'", query)
+		}
+	})
+	t.Run("Test 2", func(t *testing.T) {
+		data := []byte{
+			222, 255, 0, 100, 10, 64, 2, 0, 0, 0,
+			0, 0, 0, 100, 6, 0, 0, 0, 0, 0, 0, 255,
+			61, 100, 170, 0, 0,
+		}
+		f := NewConsumer(data)
+		query, err := f.GetSQLString()
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(query)
+	})
 }
